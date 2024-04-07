79af7548da989eb6f8b1aa80d0c2fbd87fa821c5
Johan Alejandro Peña <johanpena998@gmail.com>
2024-04-06 21:14:07 -0300

    add images

--- src/app/components/ProductInfo/Breacrumb/Breacrumb.jsx
+++ src/app/components/ProductInfo/Breacrumb/Breacrumb.jsx
@@ -1,4 +1,9 @@
+import classes from "./StyleBreacrumb.module.css";
 const Breacrumb = () => {
-  return <div>Hola</div>;
+  return (
+    <div>
+      <h5 className={classes.titulo}>INICIO / PROTEÍNAS / WHEY PROTEIN</h5>
+    </div>
+  );
 };
 export default Breacrumb;
--- src/app/components/ProductInfo/Galerias/Galeria.jsx
+++ src/app/components/ProductInfo/Galerias/Galeria.jsx
@@ -1,4 +1,10 @@
+import imagen1 from "../../images/principal.png";
+import classes from "./StyleGaleria.module.css";
 const Galeria = () => {
-  return <>Galeria 1</>;
+  return (
+    <div className={classes.principal}>
+      <img className={classes.img1} src={imagen1} alt="Imagen Principal" />
+    </div>
+  );
 };
 export default Galeria;
